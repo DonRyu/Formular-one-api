926391075257cb9e99ff6596b24cb3d9643abd12
DonRyu <alpharyu1031@gmail.com>
2023-04-18 18:32:52 -0700

    repo error

--- Core/Repositories/GenericRepository.cs
+++ Core/Repositories/GenericRepository.cs
@@ -2,34 +2,47 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Formular_one_api.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace Formular_one_api.Core.Repositories
 {
     public class GenericRepository<T> : IGenericRepository<T> where T : class
     {
-        public Task<bool> Add(T entity)
+        protected ApiDbContext _context;
+        internal DbSet<T> _dbSet;
+        protected readonly ILogger _logger;
+
+        public GenericRepository(ApiDbContext context, ILogger logger)
         {
-            throw new NotImplementedException();
+            _context = context;
+            _logger = logger;
+            this._dbSet = _context.Set<T>();
+        }
+        public async Task<IEnumerable<T>> All()
+        {
+            return await _dbSet.ToListAsync();
+        }
+        public async Task<T?> GetById(int id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+        public async Task<bool> Add(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+            return true;
         }
 
-        public Task<IEnumerable<T>> All(int id)
+        public async Task<bool> Update(T entity)
         {
-            throw new NotImplementedException();
+            _dbSet.Update(entity);
+            return true;
         }
 
-        public Task<bool> Delete(T entity)
+        public async Task<bool> Delete(T entity)
         {
-            throw new NotImplementedException();
-        }
-
-        public Task<T> GetById()
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<bool> Update(T entity)
-        {
-            throw new NotImplementedException();
+            _dbSet.Remove(entity);
+            return true;
         }
     }
 }
